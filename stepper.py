9457b611710b3bea279856ba14c098e6a941e58c
Piotr Sarna <sarna@scylladb.com>
2019-03-31 22:17:19 +0200

    Update stepper.py

--- stepper.py
+++ stepper.py
@@ -7,19 +7,15 @@ coil_A_1_pin = 4
 coil_A_2_pin = 17
 coil_B_1_pin = 23
 coil_B_2_pin = 24
-enable_pin = 18
  
 
 delay = 0.001
-count = 20480
+count = 512
 
-GPIO.setup(enable_pin, GPIO.OUT)
 GPIO.setup(coil_A_1_pin, GPIO.OUT)
 GPIO.setup(coil_A_2_pin, GPIO.OUT)
 GPIO.setup(coil_B_1_pin, GPIO.OUT)
 GPIO.setup(coil_B_2_pin, GPIO.OUT)
- 
-GPIO.output(enable_pin, 1)
  
 def do_step(w1, w2, w3, w4):
     GPIO.output(coil_A_1_pin, w1)
@@ -32,15 +28,11 @@ def forward(sequence, delay, steps):
         for step in sequence:
             do_step(*step)
             time.sleep(delay)
+
+sequence = [[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1]]
  
 if __name__ == '__main__':
-    sequence = [[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1]]
     if len(sys.argv) > 1:
         if sys.argv[1] == 'rev':
             sequence = [s for s in reversed(sequence)]
-        if sys.argv[1] == 'off':
-            do_step(0, 0, 0, 0)
-            GPIO.setup(enable_pin, 0)
-            sys.exit(0)
-    while True:
-        forward(sequence, delay, count)
+    forward(sequence, delay, count)
